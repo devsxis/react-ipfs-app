e3ad326a49a8b0e1d8d878b256257c7e3cc84b14
AbdulRehman-DEVXIS <157360781+AbdulRehmanXis@users.noreply.github.com>
2024-06-24 14:30:16 +0500

    change

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Ensure this is set correctly
+    outDir: 'Dist', // Ensure this is set correctly
   },
 });
